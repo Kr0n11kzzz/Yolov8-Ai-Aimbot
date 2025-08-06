9e0c93d744acb80ba1bee21cef6330816e010428
Kr0n11kzzz <marceviciusjokubas@gmail.com>
2025-08-06 21:44:40 +0100

    Delete test_interception.py

--- test_interception.py
+++
@@ -1,6 +0,0 @@
-from pyinterception import Interception, InterceptionMouse
-interception = Interception()
-mouse = InterceptionMouse()
-interception.set_filter(mouse.device, Interception.FILTER_MOUSE_ALL)
-print("Moving mouse right by 50 units…")
-mouse.move(50, 0, mouse.device)
